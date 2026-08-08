5cc10480edf1ae7930bdc2c2616ca0921099dadc
Hemanth Kumara V <vhemanthkumara@gmail.com>
2026-08-08 21:21:50 +0530

    Add files via upload

---
+++ tests/SearchItems.spec.ts
@@ -0,0 +1,18 @@
+import { test } from "@playwright/test"
+import { TestConfig } from "../test.config";
+import { SearchResultsPage } from "../pages/SearchResultsPage"
+
+
+test.beforeEach(async ({ page }) => {
+    const config = new TestConfig();
+    await page.goto(config.appUrl);
+})
+
+test.afterEach(async ({ page }) => {
+    await page.close();
+})
+
+test("Search any item", async ({ page }) => {
+    const searchingnyItem = new SearchResultsPage(page);
+    await searchingnyItem.SearchItem("MacBook Air");
+})
