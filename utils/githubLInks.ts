32808839d5d20c661b527b8fb1a5b143a41d651b
Hemanth Kumara V <vhemanthkumara@gmail.com>
2026-08-10 02:54:30 +0530

    Your change description

--- utils/githubLInks.ts
+++ utils/githubLInks.ts
@@ -1,1 +1,2 @@
-const Opencart_Framework= "https://github.com/vhemanthkumara/Opencart_Framework.git"
+let  Opencart_Framework= "https://github.com/vhemanthkumara/Opencart_Framework.git"
+let  Opencart_Framework_Main = "https://github.com/vhemanthkumara/Opencart_Framework-Main.git"
